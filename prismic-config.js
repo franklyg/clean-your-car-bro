be632ba285b5951eec75ea258c0f899ffbc35a7d
franklyg <f.gisonni@gmail.com>
2020-07-13 13:31:30 -0400

    updated config

--- prismic-config.js
+++ prismic-config.js
@@ -1,28 +1,16 @@
 // In src/prismic-configuration.js
 export const linkResolver = (doc) => {
 
-  if (doc.type === 'frames') {
-    return `/frames/${doc.uid}`
+  if (doc.type === 'interior-cleaning') {
+    return `/interior-cleaning/${doc.uid}`
   }
 
-  if (doc.type === 'wheels') {
-    return `/wheels/${doc.uid}`
+  if (doc.type === 'exterior-cleaning') {
+    return `/exterior-cleaning/${doc.uid}`
   }
 
-  if (doc.type === 'cranksets') {
-    return `/cranksets/${doc.uid}`
-  }
-
-  if (doc.type === 'handlebars') {
-    return `/handlebars/${doc.uid}`
-  }
-
-  if (doc.type === 'saddles') {
-    return `/saddles/${doc.uid}`
-  }
-
-  if (doc.type === 'accessories') {
-    return `/accessories/${doc.uid}`
+  if (doc.type === 'cleaning-accessories') {
+    return `/cleaning-accessories/${doc.uid}`
   }
   // Backup for all other types
   return '/'
